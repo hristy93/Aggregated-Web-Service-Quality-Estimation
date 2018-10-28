0ae532b16024daf096200907b11ed7be6d5851ab
hristy93 <hristy93@gmail.com>
2018-10-28 11:41:09 +0200

    Added a LineChart component and test data for it

---
+++ AggregatedWebServiceQualityEstimation/ClientApp/src/TestData.js
@@ -0,0 +1,19 @@
+﻿const responseTimeTestData = [
+    {
+        name: '18:40:30', value: 0
+    },
+    {
+        name: '18:40:35', value: 0.03633786
+    },
+    {
+        name: '18:40:40', value: 0.03473354
+    },
+    {
+        name: '18:40:45', value: 0.0335774
+    },
+    {
+        name: '18:40:50', value: 0.03793961
+    }
+];
+
+export { responseTimeTestData };
---
+++ AggregatedWebServiceQualityEstimation/ClientApp/src/components/common/LineChart.js
@@ -0,0 +1,31 @@
+﻿import React, { Component } from 'react';
+import { LineChart as LineChartRecharts, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Line} from 'recharts'
+
+class LineChart extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <LineChartRecharts
+                width={this.props.width || 730}
+                height={this.props.height || 250}
+                data={this.props.data}
+                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey={this.props.dataKey} />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line
+                    type="monotone"
+                    dataKey={this.props.dataKey}
+                    stroke="#8884d8" />
+            </LineChartRecharts>
+        );
+    }
+}
+
+export default LineChart;
